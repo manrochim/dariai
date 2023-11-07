54f39ba424293e60f72638b323568f133d9dafca
manrochim <40468867+manrochim@users.noreply.github.com>
2023-11-08 02:28:43 +0700

    Update killmob.js

--- killmob.js
+++ killmob.js
@@ -1,24 +1,22 @@
-
-  if (window.innerWidth <= 800) {
-    document.addEventListener("DOMContentLoaded", function() {
-      var targetElementId = localStorage.getItem("targetElementId");
-      if (targetElementId) {
-        var targetElement = document.getElementById(targetElementId);
-        if (targetElement) {
-          targetElement.scrollIntoView({ behavior: "smooth" });
-          localStorage.removeItem("targetElementId");
-        }
+if (window.innerWidth <= 800) {
+  document.addEventListener("DOMContentLoaded", function() {
+    var targetElementId = localStorage.getItem("targetElementId");
+    if (targetElementId) {
+      var targetElement = document.getElementById(targetElementId);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+        localStorage.removeItem("targetElementId");
       }
+    }
 
-      var internalLinks = document.querySelectorAll('a[href^="#"]');
-      internalLinks.forEach(function(link) {
-        link.addEventListener("click", function(event) {
-          event.preventDefault();
-          var targetId = this.getAttribute("href").substring(1);
-          localStorage.setItem("targetElementId", targetId);
-          window.location.href = this.href;
-        });
+    var internalLinks = document.querySelectorAll('a[href^="#"]');
+    internalLinks.forEach(function(link) {
+      link.addEventListener("click", function(event) {
+        event.preventDefault();
+        var targetId = this.getAttribute("href").substring(1);
+        localStorage.setItem("targetElementId", targetId);
+        window.location.href = this.href;
       });
     });
-  }
-
+  });
+}
